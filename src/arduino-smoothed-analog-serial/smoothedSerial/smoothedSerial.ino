097f3c3647432c7dc34c2ba5e59b49a09ed357d3
h <h@h.hh>
2016-06-16 16:11:19 +0300

    Improved frame rate settings for data visualizer + simple noise suppression for analog readings in Arduino

---
+++ src/arduino-smoothed-analog-serial/smoothedSerial/smoothedSerial.ino
@@ -0,0 +1,34 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+*/
+
+int minV = 1023;
+int maxV = 0;
+float smoothedValue = 0.0;
+float smoothingWeight = 0.004;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  if (sensorValue > maxV) {maxV = sensorValue;}
+  if (sensorValue < minV) {minV = sensorValue;}
+  sensorValue = map(sensorValue, minV, maxV, 0, 1000);
+  smoothedValue = smoothingWeight*sensorValue + (1 - smoothingWeight)*smoothedValue;
+  
+  // print out the value you read:
+  //Serial.println();
+  Serial.println(smoothedValue);
+  delay(1);        // delay in between reads for stability
+}

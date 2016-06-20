501ce2d099696d05ed1f821b56390b8f90cc48c1
Elmo von Weissenberg <elmo.weissenberg@gmail.com>
2016-06-20 11:02:08 +0300

    More blender test code

--- Blender/Torso/test.py
+++ Blender/Torso/test.py
@@ -22,8 +22,8 @@ bMax = 0        #Real time calibrated max value for sensor readings
 #USB port defining. First variable is the used port and second is baud rate (Arduino baud rate).
 #Uncomment the one you use
 
-ser=serial.Serial('COM3',9600)              #For Windows
-#ser=serial.Serial('/dev/ttyACM0',9600)     #For Linux
+#ser=serial.Serial('COM3',9600)              #For Windows
+ser=serial.Serial('/dev/ttyACM2',9600)     #For Linux
 
 
 #Function for reading serial data.
@@ -48,5 +48,6 @@ while(ser.inWaiting()!=0):
         print("a:{:.2f} aMin:{:.2f} aMax:{:.2f} b:{:.2f} bMin:{:.2f} bMax:{:.2f}".format(a,aMin,aMax,b,bMin,bMax))
      except:
         k=9001
+        print("OVER 9000")
     
        

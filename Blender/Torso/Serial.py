a686d669a40a8907c394efdd62fc9a706b63eafb
Ilpo Härkönen <muhnuttaja@gmail.com>
2016-06-21 22:03:15 +0300

    Rendering fixed
    Torso now renders correctly in the game engine.

--- Blender/Torso/Serial.py
+++ Blender/Torso/Serial.py
@@ -1,13 +1,9 @@
-"""
-Code currently works for one sensor measured with Arduinos analog read.
-"""
-
-
 import serial
 import math
 import bge
 from math import *
 import mathutils
+import time
 
 #potikan aariarvot: 0-1023
 
@@ -17,35 +13,62 @@ import mathutils
 a=0.0           #Sensor reading
 aMin = 9999     #Real time calibrated min value for sensor readings
 aMax = 0        #Real time calibrated max value for sensor readings
+b=0.0           #Sensor reading
+bMin = 9999     #Real time calibrated min value for sensor readings
+bMax = 0        #Real time calibrated max value for sensor readings
 
 
 #USB port defining. First variable is the used port and second is baud rate (Arduino baud rate).
 #Uncomment the one you use
 
-ser=serial.Serial('COM3',9600)              #For Windows
+#ser=serial.Serial('COM4',9600)              #For Windows
 #ser=serial.Serial('/dev/ttyACM0',9600)     #For Linux
 
 
 #Function for reading serial data.
 #Needs to be enabled for one of the objects in scene (at the Logic Editor tab).
 #This updates the read serial value in a global variable.
-
+"""
 def AnaLoop():
-     while(ser.inWaiting()!=0):
-         global a, aMin, aMax
-         line=ser.readline()
-         try:
-            a=float(line)
-         except ValueError:
-            k = 42
-         if a > aMax:
-             aMax = a
-         if a < aMin:
-             aMin = a
-         #print("a:{:.2f} aMin:{:.2f} aMax:{:.2f}".format(a,aMin,aMax))
-       
+   while(1==1):
+     global a, aMin, aMax, b, bMin, bMax
+     line=ser.readline()
+     line=line.decode("utf-8")
+     try:
+        osat=line.split(" ") 
+        a=float(osat[0])
+        b=float(osat[1])
+        if a > aMax:
+           aMax = a
+        if a < aMin:
+           aMin = a  
+        if b > bMax:
+           bMax = b
+        if b < bMin:
+           bMin = b
+        #print("a:{:.2f} aMin:{:.2f} aMax:{:.2f} b:{:.2f} bMin:{:.2f} bMax:{:.2f}".format(a,aMin,aMax,b,bMin,bMax))
+        time.sleep(0.1)
+     except:
+        k=9001
+        print("OVER 9000")
+"""       
         
         
+def AnaLoop():                
+     a=a+1    
+     global a                    
+     #print (a)
+     b = b + 1
+     aMin = 2
+     aMax = 3
+     bMin = 2
+     bMax = 3
+     global aMin
+     global bMin
+     global aMax
+     global bMax
+     global b 
+"""
 #Simple test script for rotating an object.
 def Cube():
     global a
@@ -54,12 +77,13 @@ def Cube():
     own = cont.owner   
  
     xyz = own.localOrientation.to_euler()   #Extract the Rotation Data    
+    a = 2
     xyz[0] = math.radians(a)                #PreLoad your RX data
                                             #xyz[0] x Rotation axis
                                             #xyz[1] y Rotation axis
                                             #xyz[2] z Rotation axis
     own.localOrientation = xyz.to_matrix()  #Apply your rotation data
-    
+"""   
     
     
 
@@ -78,30 +102,30 @@ R_Arm_Inner
 R_Arm_Outer
 """
     
-def Luusto():
+def BoneRot():
+    """
+    scene = bge.logic.getCurrentScene()
+    # Helper vars for convenience
+    source = scene.objects
+
+    # Get the whole Armature
+    main_arm = source.get('Luusto')
+
+    main_arm.channels['Neck'].joint_rotation[30,25,0]
+
+    main_arm.update()
+    """
     #a:n arvot flex-sensorin testauksella n.400-800
-    global a, aMin, aMax
+    global a, aMin, aMax, b, bMin, bMax
     scene = bge.logic.getCurrentScene()
     source = scene.objects
     arm = source.get('Luusto')
     
     ob = bge.logic.getCurrentController().owner
     
-    b = -((a-aMin)/(aMax - aMin))
-    ob.channels['Bone.001'].joint_rotation = mathutils.Vector([0,0,b])
+    a_rot = ((a-aMin)/(aMax - aMin))
+    a_rot = 30
+    ob.channels['Neck'].joint_rotation = mathutils.Vector([a_rot,0,0])
     ob.update()
     
-    
-#Non-working function for changing and objects color.
-def Cylinder():
-    global a, aMin, aMax
-    scene = bge.logic.getCurrentScene()
-    source = scene.objects
-    
-    ob = bge.logic.getCurrentController().owner
-    
-    b = ((a-aMin)/(aMax - aMin))
-    color = b/aMax
-    obj.color = [ 0, 0.0, 0.0, color]
-    ob.update()
     

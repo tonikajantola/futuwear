e6e0d6aba2526672098b1589da9bedabba89654c
Elmo von Weissenberg <elmo.weissenberg@gmail.com>
2016-06-21 14:32:58 +0300

    Working analog sensor data upload test from NodeMCU to Vör

--- src/arduino-client/arduino-client.ino
+++ src/arduino-client/arduino-client.ino
@@ -1,10 +1,10 @@
 #include <ESP8266WiFi.h>
 #include "cJSON.h"
 #include <string.h>
+#include <stdarg.h>
 
-
-const char* ssid     = "Eino-Huawei-Y6";
-const char* password = "jeejeejoojoo";
+const char* ssid     = "aalto open";
+const char* password = "";
 
 
 /*
@@ -13,8 +13,13 @@ const char* password = "isitfriday";
 */
 
 const char* host = "futuwear.tunk.org";
-const char* jsonData = "data={\"timestamp\":1466076661337,\"input\":\"alex on nortti\"}";
 
+void s_printf(char *buf, char *fmt, ... ){
+        va_list args;
+        va_start (args, fmt );
+        vsnprintf(buf, 128, fmt, args);
+        va_end (args);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -42,8 +47,10 @@ void setup() {
 
 int value = 0;
 
+
 void loop() {
-  delay(5000);
+  bool isAlive = true;
+  delay(100);
   ++value;
 
   Serial.print("connecting to ");
@@ -51,48 +58,56 @@ void loop() {
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 8080;
+  const int httpPort = 13337;
+  
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
   }
-  
-  Serial.println(String("### DEBUG: Content-Length:") + strlen(jsonData));
-  
-  // This will send the request to the server
-  client.print(String("POST ") + "/" + " HTTP/1.1\r\n" +
-               "Host: " + host + ":" + httpPort + "\r\n" + 
-               "Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n" +
-               "Content-Length: " + strlen(jsonData) + "\r\n" +
-               "Connection: close\r\n\r\n" + jsonData);
 
-  Serial.println("----------------------------------------------------");
-  
-  Serial.println(String("POST ") + "/" + " HTTP/1.1\r\n" +
-               "Host: " + host + ":" + httpPort + "\r\n" + 
-               "Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n" +
-               "Content-Length: " + strlen(jsonData) + "\r\n" +
-               "Connection: close\r\n\r\n" + jsonData);
+  while(isAlive) {
+    int sensorValue = analogRead(A0);
+    int timeOffset = 0;
+    // char* jsonData = "{\"sensors\":1466076661337,\"input\":\"alex on nortti\"}";
+    char jsonData[256];
+    // (String("{\"sensors\":{\"name\":\"flex1\",\"collection\":[{\"value\":") + String(sensorValue) + String(",\"timestamp\":") + String(timeOffset) + String("}]}}")).c_str();
+    s_printf(jsonData, "{\"sensors\":{\"name\":\"flex1\",\"collection\":[{\"value\":%d,\"timestamp\":%d}]}}", sensorValue, timeOffset);
+    Serial.println(String("### DEBUG: Content-Length:") + strlen(jsonData));
+    
+    // This will send the request to the server
+    String request = (String("POST ") + "/messages/" + " HTTP/1.1\r\n" +
+                 "Host: " + host + ":" + httpPort + "\r\n" + 
+                 "Content-Type: application/json; charset=UTF-8\r\n" +
+                 "Content-Length: " + strlen(jsonData) + "\r\n" +
+                 "Connection: keep-alive\r\n\r\n" + jsonData);
 
-  Serial.println("----------------------------------------------------");
-               
+    Serial.println("Sending request...");
+    client.print(request);
+    Serial.println("----------------------------------------------------");
+    
+    Serial.println(request);
   
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
+    Serial.println("----------------------------------------------------");
+                 
+    Serial.println("Waiting for response");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 400) {
+        Serial.println(">>> Client Timeout !");
+        Serial.println("closing connection");
+        client.stop();
+        isAlive = false;
+        break;
+      }
     }
+    Serial.println("Reading response");
+    timeout = millis();
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available() && (millis() - timeout < 100)){
+      String line = client.readString();
+      Serial.print(line);
+    }
+    Serial.println("Stopped reading");
   }
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
 }
 

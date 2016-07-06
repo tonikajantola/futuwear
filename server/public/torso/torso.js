403f0fc43c4792e0f383e3cfc2ec47f3bde8bfb3
Ilpo Härkönen <muhnuttaja@gmail.com>
2016-07-06 15:03:12 +0300

    3D Rotation command overhaul
    Vanha logic nodeihin perustuva logiikka heitetty pois ja siirrytty JS-muotoisiin rotate käskyihin, joilla voi operoida suoraan luita.

--- server/public/torso/torso.js
+++ server/public/torso/torso.js
@@ -1,70 +1,85 @@
-"use strict"
+ "use strict"
 
-// register the application module
 b4w.register("torso", function(exports, require) {
 
-	// import modules used by the app
-	var m_app       = require("app");
-	var m_data      = require("data");
-	var m_logn		= require("logic_nodes");
+var m_app    = require("app");
+var m_data   = require("data");
+var m_scs    = require("scenes");
+var m_armat  = require("armature");
+//var m_tsr    = require("tsr");
 
-	/**
-	 * export the method to initialize the app (called at the bottom of this file)
-	 */
-	exports.init = function() {
-		m_app.init({
-			canvas_container_id: "canvas_cont",
-			callback: init_cb,
-			show_fps: true,
-			console_verbose: true,
-			autoresize: true
-		});
-	}
 
-	/**
-	 * callback executed when the app is initialized 
-	 */
-	function init_cb(canvas_elem, success) {
 
-		if (!success) {
-			console.log("b4w init failure");
-			return;
-		}
-		load();
-	}
 
-	/**
-	 * load the scene data
-	 */
-	function load() {
-		m_data.load("torso.json", load_cb);
-	}
 
-	/**
-	 * callback executed when the scene is loaded
-	 */
-	function load_cb(data_id) {
-		m_app.enable_camera_controls();
+exports.init = function() {
+    m_app.init({
+        canvas_container_id: "canvas_cont",
+        callback: init_cb,
+        show_fps: true,
+        autoresize: true,
+        console_verbose: true
+    });
+}
 
-		// place your code here
-		m_logn.append_custom_callback("get_rotation", rotval);
-		m_logn.append_custom_callback("shoulder_Z", rot_R_Shoulder_Z);
-	}
+function init_cb(canvas_elem, success) {
 
-	function rotval(in_params, out_params) {
-		var scale = customRotation || Math.random();
-		out_params[0] = scale * 1000;
+    if (!success) {
+        console.log("b4w init failure");
+        return;
+    }
+    load();
+}
+
+function load() {
+    m_data.load("torso.json", load_cb);
+}
+
+function load_cb(data_id) {
+    m_app.enable_camera_controls();
+}
+
+function rotate_all() {
+	/*
+	rotate_bone("Bone");
+	rotate_bone("Bone.001");
+	rotate_bone("Bone.002");
+	rotate_bone("Bone.003");
+	*/
+	rotate_bone("Bone.004");
+	
+	rotate_bone("Bone.005");
+	/*
+	rotate_bone("Bone.006");
+	rotate_bone("Bone.007");
+	rotate_bone("Bone.008");
+	rotate_bone("Bone.009");
+	*/
+	
 	}
 	
-	function rot_R_Shoulder_Z(in_params, out_params) {
-		var scale = customRotation || Math.random();
-		out_params[0] = scale * 1000;
+
+function rotate_bone(bone_name) {
+	
+	var rig = m_scs.get_object_by_name("Armature");
+	var x = rotval();
+	//var tsr=[Tx,Ty,Tz,S,Rx,Ry,Rz,Rw];
+	var tsr = [0,  0, 0,1, 0, x, 0, 1];
+	m_armat.set_bone_tsr_rel(rig, bone_name, tsr);
+	
+	
+	//set_bone_tsr(armobj, bone_name, tsr);
+	//set_bone_tsr(rig,"Bone.001",trans);
+	
 	}
 
 
+function rotval() {
+		var scale = (Math.random() - 0.5)/2;
+		return scale;
+	}
+
+setInterval(function () { rotate_all() }, 300);
+	
 });
-
-var customRotation = false;
-
-// import the app module and start the app by calling the init method
-b4w.require("torso").init();
+b4w.require("torso").init(); 

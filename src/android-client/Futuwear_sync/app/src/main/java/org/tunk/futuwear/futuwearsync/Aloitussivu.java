671ca291cb2e9514bff35d1642414d98f81db118
Eino Virtanen <eino.vir@gmail.com>
2016-07-11 15:23:05 +0300

    list paired devices

--- src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
+++ src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
@@ -1,11 +1,19 @@
 package org.tunk.futuwear.futuwearsync;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Spinner;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
 
 public class Aloitussivu extends AppCompatActivity {
 
@@ -14,22 +22,15 @@ public class Aloitussivu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aloitussivu);
+        //setContentView(R.layout.activity_aloitussivu);
     }
 
 
     protected void onResume() {
         super.onResume();
-
         setContentView(R.layout.activity_aloitussivu);
 
-        TextView status = (TextView) findViewById(R.id.statusText);
-
-        status.setTextColor(Color.BLACK);
-
-        if (DEBUG) {
-            status.setText("Initialized (onResume)");
-        }
+        TextView console = (TextView) findViewById(R.id.statusText);
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -38,6 +39,39 @@ public class Aloitussivu extends AppCompatActivity {
             startActivityForResult(enableBtIntent, 3);
         }
 
+        if (mBluetoothAdapter == null) {
+            console.append("ERROR: device does not support Bluetooth!");
+        }
+
+        console.append("\nOur Bluetooth address: " + mBluetoothAdapter.getAddress());
+
+        Spinner dropDownMenu = (Spinner) findViewById(R.id.spinneri);
+
+        //Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.testi_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        dropDownMenu.setAdapter(adapter);
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        for (BluetoothDevice device : pairedDevices) {
+            // Add the name and address to an array adapter to show in a ListView
+            console.append("\nPaired device: " + device.getName() + " (" + device.getAddress() + ")");
+        }
+/*
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                adapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            log("\nERROR: no paired devices!");
+        }
+*/
     }
 
 }

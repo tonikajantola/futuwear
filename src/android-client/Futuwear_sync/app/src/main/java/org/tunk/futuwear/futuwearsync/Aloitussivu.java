9a01909d8cc369f7fce9f57d7884edb7b16f74af
Eino Virtanen <eino.vir@gmail.com>
2016-07-11 12:58:33 +0300

    Disabling bluetooth and returning to app prompts bluetooth enable screen

--- src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
+++ src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
@@ -1,13 +1,43 @@
 package org.tunk.futuwear.futuwearsync;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Aloitussivu extends AppCompatActivity {
+
+    boolean DEBUG = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aloitussivu);
     }
+
+
+    protected void onResume() {
+        super.onResume();
+
+        setContentView(R.layout.activity_aloitussivu);
+
+        TextView status = (TextView) findViewById(R.id.statusText);
+
+        status.setTextColor(Color.BLACK);
+
+        if (DEBUG) {
+            status.setText("Initialized (onResume)");
+        }
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 3);
+        }
+
+    }
+
 }

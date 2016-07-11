64e1dbc71c120ea3597ee5e7c4ac8607655d5c4a
Eino Virtanen <eino.vir@gmail.com>
2016-07-11 11:31:39 +0300

    Android client initial commit

---
+++ src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
@@ -0,0 +1,13 @@
+package org.tunk.futuwear.futuwearsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aloitussivu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aloitussivu);
+    }
+}

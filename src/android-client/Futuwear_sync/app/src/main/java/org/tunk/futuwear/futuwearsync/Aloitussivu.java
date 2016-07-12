1c9586a03d98908e14f15d48b4924f29f75c7afd
Eino Virtanen <eino.vir@gmail.com>
2016-07-12 15:02:58 +0300

    code styling

--- src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
+++ src/android-client/Futuwear_sync/app/src/main/java/org/tunk/futuwear/futuwearsync/Aloitussivu.java
@@ -1,5 +1,4 @@
 package org.tunk.futuwear.futuwearsync;
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
@@ -15,46 +14,34 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
-
 public class Aloitussivu extends AppCompatActivity {
-
     private boolean DEBUG = true;
     private TextView console;
     private TextView macAddress;
     private BluetoothAdapter mBluetoothAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aloitussivu);
-
-        console = (TextView)findViewById(R.id.statusText);
-        macAddress = (EditText)findViewById(R.id.macAddress);
+        console = (TextView) findViewById(R.id.statusText);
+        macAddress = (EditText) findViewById(R.id.macAddress);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        macAddress.setText("00:00:00:00:00:00");
-
+        macAddress.setText("90:A4:DE:6A:66:2E"); // Eino's laptop
     }
     @Override
     protected void onResume() {
-
         super.onResume();
         checkBluetoothState();
         getPairedDevices();
-
-
     }
     void getPairedDevices() {
-
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
             console.append("\nPaired devices:");
             for (BluetoothDevice device : pairedDevices) {
                 console.append("\n   " + device.getName() + " (" + device.getAddress() + ")");
             }
-        }
-        else {
+        } else {
             console.append("\nERROR: No paired devices found! Please pair your shirt with this device.");
         }
     }
@@ -63,11 +50,9 @@ public class Aloitussivu extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 3);
         }
-
         if (mBluetoothAdapter == null) {
-            console.setText("ERROR: device does not support Bluetooth!");
+            console.setText("ERROR: Bluetooth disabled!");
         }
-
-        console.setText("\nOur Bluetooth address: " + mBluetoothAdapter.getAddress());
+        console.setText("Our Bluetooth address: " + mBluetoothAdapter.getAddress());
     }
 }

7ab47bcc2cd1d4905526deeea8c374d71d966689
Elmo von Weissenberg <elmo.weissenberg@gmail.com>
2016-07-20 14:00:07 +0300

    Teensy now generates a permanent UUID for identification

---
+++ src/arduino-bluetooth/uuid.h
@@ -0,0 +1,20 @@
+#ifndef UUID_H
+#define UUID_H
+
+#include <Arduino.h>
+#include <stdbool.h>
+#include <avr/eeprom.h>
+#include <string.h>
+#include <TrueRandom.h>
+
+#define UUID_CHECK_STRING "LOLS"
+
+extern uint8_t eeprom_device_uuid_generated_check[4];
+extern uint8_t eeprom_device_uuid_str[36];
+
+extern char DEVICE_UUID_STR[37];
+
+void generate_uuid(); //Randomly generate a fresh one
+bool is_uuid_generated();
+void fetch_uuid(); //Read from memory, or generate it
+#endif
